ba3041ba404c478dabe2aa1aa8021790d5623969
Stephen Thompson <smt3@hotmail.com>
2019-07-04 22:48:52 -0500

    added css and js code update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,10 @@ The CrystalsCollector Game
 using jQuery API 
 */
 
+$(document).ready(function () {
+    var minRndNum = 19;
+    var maxRndNum = 120;
+    var rndNum = Math.floor(Math.random() * (maxRndNum - minRndNum)) + minRndNum;
+    console.log('#random-number: ' + rndNum)
+    $('#random-number').text(rndNum);
+});
